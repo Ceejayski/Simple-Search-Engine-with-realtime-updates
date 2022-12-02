c4e1d408cff6596c4356e4038a5b7f704a224ceb
Ceejay Okoli <sirchijioke1@gmail.com>
2022-12-02 17:24:37 +0100

    Add heroku elasticsearch params

--- config/initializers/elasticquery.rb
+++ config/initializers/elasticquery.rb
@@ -1,7 +1,6 @@
 if Rails.env == 'production'
   cloud_id = ENV["ELASTIC_CLOUD_ID"]
   transport_options = { request: { timeout: 250 } }
-  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
   Searchkick.client = client = Elasticsearch::Client.new(
     cloud_id: cloud_id,
     user: 'elastic',
